4cb134c2e7cef9f8103bd7c27a578ecb6b6f3951
Dave Appleton <calistralabs@gmail.com>
2017-05-03 07:07:20 +0800

    Should work in doc

--- hashBaby.go
+++ hashBaby.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
 //start main OMIT
+// only if you have ethereum libs installed!
 func prKeccak(what string) {
 	fmt.Println(crypto.Keccak256Hash([]byte(what)).Hex())
 }
 
+func GetMD5Hash(text string) {
+	hasher := md5.New()
+	hasher.Write([]byte(text))
+	fmt.Println(hex.EncodeToString(hasher.Sum(nil)))
+}
+
 func main() {
-	prKeccak("Young man, in mathematics you don't understand things. You just get used to them.")
-	prKeccak("Young man. in mathematics you don't understand things. You just get used to them.")
+	GetMD5Hash("Young man, in mathematics you don't understand things. You just get used to them.")
+	GetMD5Hash("Young man. in mathematics you don't understand things. You just get used to them.")
 }
 
 //end main OMIT
